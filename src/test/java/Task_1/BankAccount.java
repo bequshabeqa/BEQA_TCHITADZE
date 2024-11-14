f431fd8a99872dc05dfe9c4d3764f7aef98c2342
Beka Chitadze <bchitadze@credo.ge>
2024-11-14 11:07:15 +0400

    Task_1 beqa

---
+++ src/test/java/Task_1/BankAccount.java
@@ -0,0 +1,28 @@
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+    private String accountHolderName;
+
+    public BankAccount(String accountNumber, String accountHolderName) {
+        this.accountNumber = accountNumber;
+        this.balance = 0.0;
+        this.accountHolderName = accountHolderName;
+    }
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }else {
+            System.out.println("თანხა უნდა იყოს 0-ზე მეტი ");
+        }
+    }
+    public void withdraw(double amount) {
+        if (amount >0 && amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("ანგარიშზე არ არის საკმარისი თანხა! ");
+        }
+    }
+    public double getBalance() {
+        return balance;
+    }
+}
---
+++ src/test/java/Task_1/BankAccountTest.java
@@ -0,0 +1,15 @@
+public class BankAccountTest {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount("CD123456789", "BEKA TCHITADZE");
+
+        account.deposit(700);
+        account.withdraw(200);
+
+        double currencyBalance = account.getBalance();
+        if (currencyBalance == 500) {
+            System.out.println("სწორად იმუშავა, არსებული ბალანსი არის: " + currencyBalance);
+        }else {
+            System.out.println("სწორად არ იმუშავა, არსებული ბალანსი არის: " + currencyBalance);
+        }
+    }
+}
